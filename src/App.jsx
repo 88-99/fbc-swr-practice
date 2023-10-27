cc9ca8376824d165f44972b1fae87c81317f9321
88-99 <xz835.525o39@yahoo.com>
2023-10-27 21:00:56 +0900

    useStateを削除。useSWRを使ってdataで値が取れるため。

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
-import { useState } from "react";
 import useSWR from "swr";
 import "./App.css";
 
 function App() {
   const url = "https://httpstat.us/200?sleep=2000";
   const headers = { Accept: "application/json" };
+  const fetcher = () => fetch(url, { headers }).then((res) => res.json());
+  const { data, error, isLoading } = useSWR("/api/data", fetcher);
 
-  const [status, setStatus] = useState("");
-
-  const fetcher = () =>
-    fetch(url, { headers })
-      .then((res) => res.json())
-      .then((json) => setStatus(json.description));
-
-  const { error, isLoading } = useSWR("/api/sleep=2000", fetcher);
   if (error) return <div className="status">failed to load</div>;
   if (isLoading) return <div className="status">loading...</div>;
-  return <div className="status">{status && <p>Status : {status}</p>}</div>;
+  return (
+    <div className="status">{data && <p>Status : {data.description}</p>}</div>
+  );
 }
-
 export default App;
